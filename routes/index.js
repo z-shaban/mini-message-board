854976c0eac0c2a763d136b4d3dddf290bd9a108
shaban <146794146+sarzhh@users.noreply.github.com>
2025-10-15 18:53:48 +0000

    add open and back buttons

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,16 @@ const messages = [
 
 index.get('/', (req,res)=>{
     res.render('index',{messages : messages})
+  
 })
+
+index.get('/message/:id', (req,res)=>{
+    const id = req.params.id
+   const detailedMessage = messages.find((message,index)=> id == index)
+    res.render('message',{detailedMessage})}
+)
+
+
 
 index.post('/new', (req,res)=>{
    messages.push({text: req.body.message, user: req.body.name, date: new Date()})
